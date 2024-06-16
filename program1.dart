e3f569bbc1824aff24ecca0aa35c38ce18c00b6e
rahulvarma07 <rahulvarma070707@gmail.com>
2024-06-16 23:08:40 +0530

    Print Statement

---
+++ program1.dart
@@ -0,0 +1,11 @@
+// Hello World Program..
+
+void main(){
+  // The main function in dart language..
+
+  print("Hello, World!");
+  // print("Hello, World!") : print the content("Hello, World!") on the terminal..
+
+}
+
+// Try Out: Print Your name, age, Qualification etc..
